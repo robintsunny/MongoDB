cad22eebdc623703c0c1c8ccc93141142a84fc00
robin <robintsunny@gmail.com>
2018-09-24 16:38:50 -0700

    Class Model Updates

--- prod/users/test/update_test.js
+++ prod/users/test/update_test.js
@@ -41,5 +41,33 @@ describe('Updating a User', () => {
     })
 
 
+    it('A model class can update', done => {
+        User.update({name: 'Joe'}, {name: 'Alex'})
+        .then( () => User.find({})) //finds all users, no criteria was passed in
+        .then( users =>  {
+            assert(users.length === 1);
+            assert(users[0].name === 'Alex');
+            done()
+        })
+    })
 
+    it('A model class can update one record', done => {
+        User.updateOne({name: 'Joe'}, {name: 'Alex'})
+        .then( () => User.find({})) //finds all users, no criteria was passed in
+        .then( users =>  {
+            assert(users.length === 1);
+            assert(users[0].name === 'Alex');
+            done()
+        })
+    })
+
+    it('A model class can update a record by id', done => {
+        User.findByIdAndUpdate(joe._id, {name: 'Alex'})
+        .then( () => User.find({})) //finds all users, no criteria was passed in
+        .then( users =>  {
+            assert(users.length === 1);
+            assert(users[0].name === 'Alex');
+            done()
+        })
+    })
 })
