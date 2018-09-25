7249128c22a32fdba4eaba35a415759fb9a4e033
robin <robintsunny@gmail.com>
2018-09-25 14:56:04 -0700

    Validate Records

---
+++ prod/users/test/validation_test.js
@@ -0,0 +1,20 @@
+const assert = require('assert')
+const User = require('../src/user')
+
+describe('Validating Records', () => {
+    it('requires a name', () => {
+        const user = new User({name: undefined})
+        const validationResult = user.validateSync()
+        const message = validationResult.errors.name.message
+
+        assert(message === 'Name is required') 
+    })
+    
+    it('requires a user name be longer than 2 characters', () => {
+        const user = new User({name: 'Al'})
+        const validationResult = user.validateSync()
+        const message = validationResult.errors.name.message
+    
+        assert(message === 'Name must be longer than 2 characters')
+    })
+})
