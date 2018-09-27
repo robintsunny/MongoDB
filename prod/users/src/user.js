29c44a9b4e4e642c3a69483c35c3446444c1f257
robin <robintsunny@gmail.com>
2018-09-27 15:41:48 -0700

    Virtual Types

--- prod/users/src/user.js
+++ prod/users/src/user.js
@@ -13,11 +13,12 @@ const UserSchema = new Schema ({
         required: [true, 'Name is required']
     },
 
-    posts: [PostSchema]
+    posts: [PostSchema],
+    likes: Number
 })
 
 UserSchema.virtual('postCount').get(function() {
-    
+    return this.posts.length
 })
 
 const User = mongoose.model('user', UserSchema);
--- prod/users/test/update_test.js
+++ prod/users/test/update_test.js
@@ -8,7 +8,7 @@ describe('Updating a User', () => {
     beforeEach(done => {
         joe = new User({
             name: 'Joe',
-            postCount: 0
+            likes: 0
         })
 
         joe.save().then(() => {
@@ -73,11 +73,11 @@ describe('Updating a User', () => {
     })
 
 
-    xit('A user can have their postcount incremented by 1', done => {
-        User.update({ name: 'Joe' }, { $inc: { postCount: 10 } })
+    it('A user can have their postcount incremented by 1', done => {
+        User.update({ name: 'Joe' }, { $inc: { likes: 10 } })
         .then(() => User.findOne({ name: 'Joe' }))
         .then((user) => {
-            assert(user.postCount === 10);
+            assert(user.likes === 10);
             done()
         });
     });
